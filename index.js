21e5e652875772f6b8efdda3bf6d108597672006
Zachary Sain <zachsain@gmail.com>
2021-11-10 12:23:07 -0600

    commit tree 0d4ab0712f6667948bc9c7996d9b1f909bc38ad9

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function superbowlWin(arrayOfObjects) {
+    for (let index of arrayOfObjects) {
+        if (index.result === "W") {
+            return index.year;
+        }
+    }
+}
 
+
+// const result = record.find(superbowlWin)
+
+// function superbowlWin(index) {
+//     if (index.result === "W") {
+//         return index.year
+//     }
+// }
